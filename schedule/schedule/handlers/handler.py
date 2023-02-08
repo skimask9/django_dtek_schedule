a0e53c75285ac6aefffecf59d6080518b5f7f33f
skimask9 <pashapasha232@gmail.com>
2023-02-08 15:54:16 +0200

    new version of handler

--- schedule/schedule/handlers/handler.py
+++ schedule/schedule/handlers/handler.py
@@ -1,8 +1,5 @@
 from datetime import datetime
 
-DAYS = {"Friday","Sunday","Saturday","Monday"}
-
-
 
 schedule = {
     1: "06:00-08:00,14:00-16:00,22:00-00:00",
@@ -11,11 +8,23 @@ schedule = {
     4: "00:00-02:00,08:00-10:00,16:00-18:00",
 }
 
+WEEKDAYS = {
+    "Friday": 1,
+    "Sunday": 1,
+    "Saturday": 1,
+    "Mondauy": 1,
+    "Tuesday": 2,
+    "Wednesday": 3,
+    "Thursday": 4,
+}
+
 
 def schedule_in_string(day: str) -> str:
-    normalize_day = day.split(",")
-    new_day = "\n".join(normalize_day)
+    # Split the string by commas
+    new_day = "\n".join(day.split(","))
+    # Join the string with new lines
     return new_day
+
 
 
 def display_schedule(d: str):
@@ -26,12 +35,8 @@ def display_schedule(d: str):
 
 def check_schedule_day():
     exactly_day = datetime.now().strftime("%A")
-    if exactly_day in DAYS:
-       return display_schedule(schedule_in_string(schedule[1]))
-    elif exactly_day == "Tuesday":
-        return display_schedule(schedule_in_string(schedule[2]))
-    elif exactly_day == "Wednesday":
-        return display_schedule(schedule_in_string(schedule[3]))
-    elif exactly_day == "Thursday":
-        return display_schedule(schedule_in_string(schedule[4]))
+    day_schedule = schedule[WEEKDAYS[exactly_day]]
+    return display_schedule(schedule_in_string(day_schedule))
 
+
+print(check_schedule_day())
---
+++ schedule/schedule/views.py
@@ -0,0 +1,1 @@
+#make a django view with output to html template
